cfb5eb7cb5e45c01acc76b9d1c61fed6583252a7
Clyde Clark <clydeclark78@gmail.com>
2020-11-19 15:51:52 -0800

    added bootstrap styling

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -3,20 +3,29 @@ import React from "react";
 
 function EmployeeList(props) {
     const employees = props.data;
-    // let name = `${{employees.name.first}} ${{employees.name.last}}`;
+    // let name = {employees.name.first} + " " + {employees.name.last};
     console.log(employees);
     
     return (
-        <div>
-            
-            {employees.map(employee =>
-                <div class="employee">
-                    <img class="left" src={employee.picture.thumbnail} alt={`name`} />
-                    <p class="left">{employee.phone}</p>
-                    <p class="left">{employee.email}</p>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="data-left col-md-1">image</div>
+                    <div class="data-left col-md-3">name</div>
+                    <div class="data-left col-md-2">phone</div>
+                    <div class="data-left col-md-3">email</div>
+                    <div class="data-left col-md-3">date of birth</div>
                 </div>
-            )}
-
+                {employees.map(employee =>
+                    <div class="employee col-md-12">
+                        <img class="data-left col-md-1" src={employee.picture.thumbnail} alt={`name`} />
+                        <p class="data-left col-md-3">name</p>
+                        <p class="data-left col-md-2">{employee.phone}</p>
+                        <p class="data-left col-md-3">{employee.email}</p>
+                        <p class="data-left col-md-3">{employee.dob.date}</p>
+                    </div>
+                )}
+            </div>
         </div>
     
     )

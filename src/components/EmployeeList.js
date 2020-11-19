4c0366d36bfeb1a8295898d4e7f3e460b1375ddd
Clyde Clark <clydeclark78@gmail.com>
2020-11-19 11:16:31 -0800

    adding left float to employee entries

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -3,18 +3,17 @@ import React from "react";
 
 function EmployeeList(props) {
     const employees = props.data;
-    // let name = `${employees.name.first} ${employees.name.last}`;
+    // let name = `${{employees.name.first}} ${{employees.name.last}}`;
     console.log(employees);
     
     return (
         <div>
             
             {employees.map(employee =>
-                <div>
-                    <img src={employee.picture.thumbnail} alt={`name`} />
-                    {/* <p>${name}</p> */}
-                    <p>{employee.phone}</p>
-                    <p>{employee.email}</p>
+                <div class="employee">
+                    <img class="left" src={employee.picture.thumbnail} alt={`name`} />
+                    <p class="left">{employee.phone}</p>
+                    <p class="left">{employee.email}</p>
                 </div>
             )}
 

32f09db839e9e9daa3d69cfda5bfeabb66e357c1
Clyde Clark <clydeclark78@gmail.com>
2020-11-19 16:05:01 -0800

    styled column headers

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -10,11 +10,11 @@ function EmployeeList(props) {
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <div class="data-left col-md-1">Image</div>
-                    <div class="data-left col-md-3">Name</div>
-                    <div class="data-left col-md-2">Phone</div>
-                    <div class="data-left col-md-3">Email</div>
-                    <div class="data-left col-md-3">Date of Birth</div>
+                    <div class="col-head data-left col-md-1">Image</div>
+                    <div class="col-head data-left col-md-3">Name</div>
+                    <div class="col-head data-left col-md-2">Phone</div>
+                    <div class="col-head data-left col-md-3">Email</div>
+                    <div class="col-head data-left col-md-3">Date of Birth</div>
                 </div>
                 {employees.map(employee =>
                     <div class="employee col-md-12">
